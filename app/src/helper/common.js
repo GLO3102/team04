a43a504c91ab8b61676b24f33f00993ceccbb7d5
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2015-11-21 22:57:34 -0500

    Màj de l'effet de flou. Changement d'hébergeur JSON. Passage de myJSON à JSONBlob en espérant ne pas se faire supprimer. Le fichier creation>reactions-example.json contient les JSON de base à resauvegarder chez JSONBlob en cas de suppression. Les Id obtenus sont à mettre dans app>src>helper>common.js .

--- app/src/helper/common.js
+++ app/src/helper/common.js
@@ -6,9 +6,9 @@ define(function (require) {
         UMOVIE_API_BASE_URL: 'https://umovie.herokuapp.com/unsecure/',
         TMDB_API_BASE_URL: 'https://api.themoviedb.org/3/',
         TMDB_API_KEY: '8f002dc2dff6b5a18ac35ef9ca6d5d02',
-        REACTIONS_EMOJI_MOVIE_URL: 'https://api.myjson.com/bins/1qxo1',
-        REACTIONS_EMOJI_TVSHOW_URL: 'https://api.myjson.com/bins/17uxt',
-        REACTIONS_EMOJI_WATCHLIST_URL: 'https://api.myjson.com/bins/4ulq9',
+        REACTIONS_EMOJI_MOVIE_URL: 'https://jsonblob.com/api/jsonBlob/565139c6e4b01190df40ef0a',
+        REACTIONS_EMOJI_TVSHOW_URL: 'https://jsonblob.com/api/jsonBlob/565137c2e4b01190df40ef00',
+        REACTIONS_EMOJI_WATCHLIST_URL: 'https://jsonblob.com/api/jsonBlob/56513aefe4b01190df40ef10',
         VOTER_ID: 'nanashi@sekai-no-owari.umovie',
         ENTER_KEY: 13
     };
