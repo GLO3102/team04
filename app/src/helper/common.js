6a6236150ce17afa125226399a08725cd603a21e
Ermine Wankpo <erminesyas@gmail.com>
2015-12-02 10:35:42 -0500

    Affichage episodemodale

--- app/src/helper/common.js
+++ app/src/helper/common.js
@@ -4,12 +4,14 @@ define(function (require) {
 
     return {
         UMOVIE_API_BASE_URL: 'https://umovie.herokuapp.com/unsecure/',
+        UMOVIE_API_BASE_URL_SECURED: 'https://umovie.herokuapp.com/',
         TMDB_API_BASE_URL: 'https://api.themoviedb.org/3/',
         TMDB_API_KEY: '8f002dc2dff6b5a18ac35ef9ca6d5d02',
         REACTIONS_EMOJI_MOVIE_URL: 'https://jsonblob.com/api/jsonBlob/565139c6e4b01190df40ef0a',
         REACTIONS_EMOJI_TVSHOW_URL: 'https://jsonblob.com/api/jsonBlob/565137c2e4b01190df40ef00',
         REACTIONS_EMOJI_WATCHLIST_URL: 'https://jsonblob.com/api/jsonBlob/56513aefe4b01190df40ef10',
         VOTER_ID: 'nanashi@sekai-no-owari.umovie',
+        LOGIN_TOKEN_COOKIE: 'LoginToken',
         ENTER_KEY: 13
     };
 
