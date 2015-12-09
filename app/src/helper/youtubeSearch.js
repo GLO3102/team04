e3bbb42b4c9a4ab03655b9f3d07e110af4be33c7
Ermine Wankpo <erminesyas@gmail.com>
2015-12-09 16:41:33 -0500

    Ajout recherche episode, désactivation du bouton fullscreen youtube et quelques petits ajustements

--- app/src/helper/youtubeSearch.js
+++ app/src/helper/youtubeSearch.js
@@ -3,32 +3,21 @@ googleApiClientReady = function() {
     gapi.client.setApiKey('AIzaSyC2-tNBEPJZvz-nU562gJ7LH4N29-jRHMg');
 };
 
+
 function youtubeSearch(q, callback) {
     gapi.client.load('youtube', 'v3', function() {
 
         var request = gapi.client.youtube.search.list({
             q: q,
-            part: 'snippet'
+            part: 'snippet',
+            fs:'0'
         });
 
         request.execute(function(response) {
             var videoId = response.result.items[0].id.videoId;
-            callback('https://www.youtube.com/embed/' + videoId);
+            callback('https://www.youtube.com/embed/' + videoId+ '/rel=0?&fs=0');
         });
     });
 }
 
-function youtubeSearchModal(q, callback) {
-    gapi.client.load('youtube', 'v3', function() {
 
-        var request = gapi.client.youtube.search.list({
-            q: q,
-            part: 'snippet'
-        });
-
-        request.execute(function(response) {
-            var videoId = response.result.items[0].id.videoId;
-            callback('https://www.youtube.com/v/' + videoId);
-        });
-    });
-}
