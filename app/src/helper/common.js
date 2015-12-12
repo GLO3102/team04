205b0408418faed1a0e4cefa9efa73371c44553c
ericnsh <ericnsh@gmail.com>
2015-12-12 15:10:07 -0500

    add actors suggestion

--- app/src/helper/common.js
+++ app/src/helper/common.js
@@ -7,7 +7,7 @@ define(function (require) {
         UMOVIE_API_BASE_URL_SECURED: 'https://umovie.herokuapp.com/',
         TMDB_API_BASE_URL: 'https://api.themoviedb.org/3/',
         TMDB_API_KEY: '8f002dc2dff6b5a18ac35ef9ca6d5d02',
-        TASTEKID_API_KET : '185021-UMIUmovi-77VCGM7D',
+        TASTEKID_API_KEY : '185021-UMIUmovi-77VCGM7D',
         REACTIONS_EMOJI_MOVIE_URL: 'https://jsonblob.com/api/jsonBlob/565139c6e4b01190df40ef0a',
         REACTIONS_EMOJI_TVSHOW_URL: 'https://jsonblob.com/api/jsonBlob/565137c2e4b01190df40ef00',
         REACTIONS_EMOJI_WATCHLIST_URL: 'https://jsonblob.com/api/jsonBlob/56513aefe4b01190df40ef10',
